76ee6955a84d6e741e3eb9eb6dfdf49245f28432
Bryan O'Connor <bryano1993@gmail.com>
2018-08-21 12:13:54 -0700

    problem 1

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -1,10 +1,24 @@
 const request = require("request");
 const server = require("../../src/server");
 const base = "http://localhost:3000/";
-
+//a test for the route path
 describe("routes : static", () => {
   //#1
   describe("GET /", () => {
+    //#2
+    it("should return status code 200", done => {
+      //#3
+      request.get(base, (err, res, body) => {
+        expect(res.statusCode).toBe(200);
+
+        //#4
+        done();
+      });
+    });
+  });
+
+  //#1
+  describe("GET /marco", () => {
     //#2
     it("should return status code 200", done => {
       //#3
