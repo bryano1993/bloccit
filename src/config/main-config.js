14e6bef3f03affe8f39460c256230a6742be3102
Bryan O'Connor <bryano1993@gmail.com>
2018-08-21 18:29:20 -0700

    help

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -1,4 +1,16 @@
 require("dotenv").config();
+const path = require("path");
+const viewsFolder = path.join(__dirname, "..", "views");
+
+module.exports = {
+  init(app, express) {
+    app.set("views", viewsFolder);
+    app.set("view engine", "ejs");
+    app.use(express.static(path.join(__dirname, "..", "assets")));
+  }
+};
+
+require("dotenv").config();
 
 module.exports = {
   init() {}
