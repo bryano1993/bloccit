7bb165656e74c3388d89f14341bc74573bad2adc
Bryan O'Connor <bryano1993@gmail.com>
2018-08-27 08:46:07 -0700

    assignment-4-routingcontrollersandtheview:edit

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -22,7 +22,7 @@ describe("routes : static", () => {
     //#2
     it("should return status code 200 and have 'About Us.' in the body of the response", () => {
       //#3
-      request.get(base, (err, res, body) => {
+      request.get(base + "/about", (err, res, body) => {
         expect(res.statusCode).toBe(200);
         expect(body).toContain("About Us.");
 
