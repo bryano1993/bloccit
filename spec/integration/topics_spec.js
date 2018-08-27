6bbb242bde48881ad882a1217ceaa165db30d165
Bryan O'Connor <bryano1993@gmail.com>
2018-08-27 14:33:27 -0700

    edit

--- spec/integration/topics_spec.js
+++ spec/integration/topics_spec.js
@@ -24,7 +24,7 @@ describe("routes : topics", () => {
   });
 
   describe("GET /topics", () => {
-    it("should return a status code 200 and all topics", done => {
+    it("should return a status code 200 and all topics", () => {
       request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
         expect(err).toBeNull();
